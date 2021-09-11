6f9abcf32df1604bab8afd6dda728b61fae0c7f9
bitcoineazy <bitcoineazy@gmail.com>
2021-09-11 19:58:32 +0300

    File Manager - done

--- FileManager/File_Manager.py
+++ FileManager/File_Manager.py
@@ -15,26 +15,120 @@ class FileManager:
         if not os.path.exists(f'{self.root_directory}/{directory_name}'):
             os.makedirs(f'{self.root_directory}/{directory_name}')
             print(f'Директория "{directory_name}" успешно создана')
+            self.refresh_directory()
         else:
             print("Директория уже существует")
 
     def create_file(self):
         file_name = str(input("Имя файла: "))
-        new_file = open(f"{self.root_directory}/{file_name}", "w")
-        new_file.write(str(input('Введите текст для записи в файл: ')))
-        new_file.close()
+        if not os.path.exists(f'{self.root_directory}/{file_name}'):
+            new_file = open(f"{self.root_directory}/{file_name}", "w")
+            # new_file.write(str(input('Введите текст для записи в файл: ')))
+            new_file.close()
+            print(f'Файл "{file_name}" успешно создан')
+            self.refresh_directory()
+        else:
+            print('Файл с таким названием уже существует')
+
+    def overwrite_file(self):
+        file_id = int(input('Введите ID файла для записи: '))
+        location = self.id_choice(file_id)
+        try:
+            with open(f'{self.root_directory}/{location}', mode='w') as f:
+                f.write(str(input('Введите текст для записи в файл: ')))
+        except FileNotFoundError:
+            print(f'Файл с  ID = "{file_id}" не найден')
+        except IsADirectoryError:
+            print('Это директория')
+        else:
+            print('Текст успешно записан в файл')
 
     def read_file(self):
         file_id = int(input('Введите ID файла чтобы прочитать: '))
-        location = self.id_choice(file_id)
+        file_name = self.id_choice(file_id)
         try:
-            file_location = f"{self.root_directory}/{location}"
+            file_location = f"{self.root_directory}/{file_name}"
             with open(file_location, "r") as f:
                 text = f.read()
             for line in text.splitlines():
                 print(line)
         except FileNotFoundError:
-            print("Файл не найден")
+            print(f'Файл с ID = "{file_id}" не найден')
+
+    def delete_directory(self):
+        directory_id = int(input('Введите ID директории чтобы удалить: '))
+        directory_name = self.id_choice(directory_id)
+        try:
+            shutil.rmtree(f'{self.root_directory}/{location}')  # Удалить директорию и все файлы в ней
+        except OSError as e:
+            print(f'Error: {e.filename} - {e.strerror}')
+        else:
+            print(f'Директория "{directory_name}" успешно удалена')
+            self.refresh_directory()
+
+    def delete_file(self):
+        file_id = int(input('Введите ID файла чтобы удалить'))
+        file_name = self.id_choice(file_id)
+        try:
+            os.remove(f"{self.root_directory}/{file_name}")
+        except OSError as e:
+            print(f'Error: {e.filename} - {e.strerror}')
+        else:
+            print(f'Файл "{file_name}" успешно удалён')
+            self.refresh_directory()
+
+    def move_between_directories(self):  # Перемещение по разрешенным директориям
+        chosen_directory = int(input('Введите ID директории чтобы переместиться: '))
+        location = self.id_choice(chosen_directory)
+        self.root_directory += f'/{location}'
+        self.refresh_directory()
+
+    def move_up(self):  # Подняться вверх по директории
+        location = self.root_directory.split('/')
+        location.pop()
+        up = '/'.join(location)
+        self.root_directory = up
+        self.refresh_directory()
+
+    def rename_file(self):
+        file_id = int(input('Введите ID файла, чтобы переименовать: '))
+        file_to_rename = f"{self.root_directory}/{self.id_choice(file_id)}"
+        new_name = str(input('Введите новое название файла: '))
+        try:
+            os.rename(file_to_rename, f'{self.root_directory}/{new_name}')
+        except OSError as e:
+            print(f'Error: {e.filename} - {e.strerror}')
+        else:
+            print('Успешное переименование')
+            self.refresh_directory()
+
+    def copy_files(self):
+        start_id = int(input('ID директории из который производится копирование: '))
+        start_directory = f"{self.root_directory}/{self.id_choice(start_id)}"
+        end_id = int(input('ID директории чтобы скопировать в неё: '))
+        end_directory = f"{self.root_directory}/{self.id_choice(end_id)}"
+        try:
+            copy_tree(start_directory, end_directory)
+        except OSError as e:
+            print(f'Error: {e.filename} - {e.strerror}')
+        else:
+            print('Успешное копирование')
+
+    def move_files(self):
+        self.list_directory()
+        start_id = int(input('ID Файла для перемещения: '))
+        start_location = f"{self.root_directory}/{self.id_choice(start_id)}"
+        end_id = int(input('ID директории куда переместить: '))
+        end_directory = f"{self.root_directory}/{self.id_choice(end_id)}"
+        try:
+            shutil.move(start_location, end_directory)
+        except OSError as e:
+            print(f'Error: {e.filename} - {e.strerror}')
+        else:
+            print('Успешное перемещение')
+
+    def refresh_directory(self):
+        self.allowed_files = self.list_directory()[1]
 
     def list_directory(self):
         self.display_data = []
@@ -50,49 +144,6 @@ class FileManager:
                         stralign='center')
         return data, self.display_data
 
-    def list_stats(self):
-        files = os.scandir(path=self.root_directory)
-        for each in files:
-            print(os.stat(each))
-
-    def delete_directory(self):
-        directory_id = int(input('Введите ID директории чтобы удалить: '))
-        location = self.id_choice(directory_id)
-        try:
-            os.rmdir(f'{self.root_directory}/{location}')
-        except OSError as e:
-            print(f'Error: {e.filename} - {e.strerror}')
-
-    def move_between_directories(self):  # Перемещение по разрешенным директориям
-        chosen_directory = int(input('Введите ID директории чтобы переместиться: '))
-        location = self.id_choice(chosen_directory)
-        self.root_directory += f'{location}'
-
-    def move_up(self):  # Подняться вверх по директории
-        location = self.root_directory.split('/')
-        location.pop()
-        up = '/'.join(location)
-        self.root_directory = up
-
-    def rename_file(self):
-        print('Список файлов и директорий для переименовывания: ')
-        self.list_directory()
-        file_to_rename = f"{self.root_directory}/{str(input('Введите название файла, чтобы переименовать: '))}"
-        new_name = str(input('Введите новое название файла: '))
-        os.rename(file_to_rename, f'{self.root_directory}/{new_name}')
-
-    def copy_files(self):
-        self.list_directory()
-        start_directory = f"{self.root_directory}/{str(input('Директория из который производится копирование: '))}"
-        end_directory = f"{self.root_directory}/{str(input('Название новой директории, чтобы скопировать в неё: '))}"
-        copy_tree(start_directory, end_directory)
-
-    def move_files(self):
-        self.list_directory()
-        start_file = f"{self.root_directory}/{str(input('ID Файла для перемещения: '))}"
-        end_directory = f"{self.root_directory}/{str(input('В какую директорию переместить: '))}"
-        shutil.move(start_file, end_directory)
-
     def id_choice(self, object_id):
         ids = [i[2] for i in self.allowed_files]
         names = [i[1] for i in self.allowed_files]
@@ -100,10 +151,12 @@ class FileManager:
             return names[object_id - 1]
 
     def CLI(self):
-        print('Файловый менеджер\n')
+        print('-----------Файловый менеджер-----------\n')
         commands = [['1', 'Просмотр директории'], ['2', 'Создать папку'], ['3', 'Удалить папку'],
-                    ['4', 'Переместиться'], ['5', 'Запись текста в файл'], ['6', 'Просмотр текстового файла']]
-        help_page = tabulate((i for i in commands), headers=['ID', 'Метод'], tablefmt='grid', stralign='center')
+                    ['4', 'Переход между директориями'], ['5', 'Создать пустой файл'], ['6', 'Запись текста в файл'],
+                    ['7', 'Просмотр текстового файла'], ['8', 'Удалить файл'], ['9', 'Копирование из папки в папку'],
+                    ['10', 'Перемещение файлов'], ['11', 'Переименовать файл']]
+        help_page = tabulate((i for i in commands), headers=['ID', 'Метод'], tablefmt='github', stralign='center')
         print(help_page)
         while True:
             choose = str(input(
@@ -120,9 +173,17 @@ class FileManager:
             if choose == '5':
                 self.create_file()
             if choose == '6':
+                self.overwrite_file()
+            if choose == '7':
                 self.read_file()
+            if choose == '8':
+                self.delete_file()
             if choose == '9':
+                self.copy_files()
+            if choose == '10':
                 self.move_files()
+            if choose == '11':
+                self.rename_file()
             if choose.lower() == 'help':
                 print(f'\n{help_page}')
             if choose.lower() == 'exit':
